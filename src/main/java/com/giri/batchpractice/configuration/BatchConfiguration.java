3381962a5be92cce6477f9f0c50b71ea739b093f
ndgndg91 <ndgndg91@gmail.com>
2020-11-02 21:50:33 +0900

    <feat> tasklet, job parameters

---
+++ src/main/java/com/giri/batchpractice/configuration/BatchConfiguration.java
@@ -0,0 +1,47 @@
+package com.giri.batchpractice.configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Log4j2
+@Configuration
+@EnableBatchProcessing
+@RequiredArgsConstructor
+public class BatchConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job firstJob(){
+        return jobBuilderFactory.get("matchJob")
+                .start(nonBankBookOrderStep())
+                .build();
+    }
+
+    @Bean
+    public Step nonBankBookOrderStep(){
+        return stepBuilderFactory.get("nonBankBookOrderStep")
+                .tasklet(((contribution, chunkContext) -> {
+                    //    java -jar demo-0.0.1.jar "item=job-parameter-test" "run.date(date)=2020/11/01"
+                    log.info("tasklet execute!");
+                    String item = chunkContext.getStepContext()
+                            .getJobParameters().getOrDefault("item", "").toString();
+                    String runDate = chunkContext.getStepContext()
+                            .getJobParameters().getOrDefault("run.date", "").toString();
+
+                    log.info("run date : {}, item parameter : {}", runDate, item);
+                    // run date : Sun Nov 01 00:00:00 KST 2020, item parameter : job-parameter-test
+                    return RepeatStatus.FINISHED;
+                })).build();
+    }
+}
